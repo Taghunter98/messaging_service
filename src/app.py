e791334dc19b8f53fa5cb1f32f98fff6f63c8d7c
Taghunter98 <jb@vmi.tv>
2025-01-17 16:52:14 +0000

    - added cli arguments - added cli send - added cli get - added cli get chat history

--- src/app.py
+++ src/app.py
@@ -13,17 +13,70 @@ def fetch_messages(receiver_id):
     payload = {"receiver_id": receiver_id}
     
     try:
-        response = requests.get(url, params=payload)
+        response = requests.get(url, params = payload)
         response.raise_for_status() # Raise exception for HTTP
         data = response.json()
-        for message in data:
-            print(f"\t\n\nMessage from: {data.get('sender_id')}at {data.get('timestamp')}\nMessage: {data.get('message')}")
+        print(json.dumps(data, indent = 4))
             
     except requests.exceptions.RequestException as e:
         print(f"Error fetching messages: {e}")
         return None
     
+def send_message(sender_id, receiver_id, content):
+    url = f"{API_BASE_URL}/messages/send"
+    
+    response = requests.post(url, json={
+        "sender_id": sender_id,
+        "receiver_id": receiver_id,
+        "content": content
+    })
+        
+    if response.status_code == 200:
+            print("Message sent successfully.")
+    else:
+        try:
+            print(f"Failed to send: {response.json()}")
+        except ValueError:
+            print("Response is not in JSON format.")
+
+def get_chat_history(chat_id):
+    url = f"{API_BASE_URL}/messages/history"
+    
+    payload = {"chat_id": chat_id}
+    
+    try:
+        response = requests.get(url, params = payload)
+        response.raise_for_status()
+        data = response.json()
+        print(json.dumps(data, indent = 4))
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching chat history: {e}")
+        return None
+    
 if __name__ == '__main__':
-    receiver_id = 1
-    messages = fetch_messages(receiver_id)
-    print(messages)
+    # CLI argument parser
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--get", type=int, nargs=1, help="Get all messages with <user_id>")
+    parser.add_argument("--send", nargs=3, help="Send a message from <sender_id> to <reciever_id> and <content>")
+    parser.add_argument("--chat", type=str, nargs=1, help="Get chat history for <chat_id>")
+    args = parser.parse_args() # Parse arguments
+    
+    
+    # Arguments
+    if args.get:
+        receiver_id = args.get[0]
+        print(f"Fetching messages for receiver_id: {receiver_id}...")
+        messages = fetch_messages(receiver_id)
+        
+    elif args.send:
+        sender_id = int(args.send[0])
+        receiver_id = int(args.send[1])
+        content = args.send[2]
+        print(f"Sending message from sender_id: {sender_id} to receiver_id: {receiver_id} with content: {content}...")
+        send_message(sender_id, receiver_id, content)
+        
+    elif args.chat:
+        chat_id = args.chat[0]
+        print(f"Fetching chat history for id: {chat_id}...")
+        get_chat_history(chat_id)
